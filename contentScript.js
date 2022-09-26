a37bb3d8bf739ddfacab3315949775914559b5a8
Angus Zou <u3583523@connect.hku.hk>
2022-09-26 13:04:56 +0800

    add sidebar add/remove course button

--- contentScript.js
+++ contentScript.js
@@ -1,60 +1,110 @@
-// Note: every element that is to be removed during a clearing session 
+// Note: every element that is to be removed during a clearing session
 // should be marked with a "helper-extension" classname
 // Otherwise it should be marked with "helper-extension-persistent"
 
 // Code splitting was done through globalThis (which was confined within ContentScript. Thus no pollutions were made)
-mainFunction()
+mainFunction();
 
 async function mainFunction() {
-  await addCourseOfSem()
-  globalThis.addFeedbackBox()
+  await addCourseOfSem();
+  globalThis.addFeedbackBox();
   // globalThis.addMessageBox()
 }
 
 async function addCourseOfSem() {
-  courseHTML = new Array()
-  const { courseIDs } = await browser.storage.sync.get({'courseIDs': []})
+  courseHTML = new Array();
+  const { courseIDs } = await browser.storage.sync.get({ courseIDs: [] });
 
-  clearAll()
-  var courses = document.getElementsByClassName("coursebox")
+  clearAll();
+  var courses = document.getElementsByClassName("coursebox");
   for (var i = 0; i < courses.length; i++) {
-    currentCourseID = courses[i].dataset.courseid
-    var included = false
-    if (courseIDs){ included = courseIDs.includes(currentCourseID) }
+    currentCourseID = courses[i].dataset.courseid;
+    var included = false;
+    if (courseIDs) {
+      included = courseIDs.includes(currentCourseID);
+    }
     if (included) {
       //如果在列表中
       //复制element，存入数组
-      courseHTML.push(courses[i].cloneNode(true))
+      courseHTML.push(courses[i].cloneNode(true));
 
       // Applies to all courses on the page that is in the list (in "my courses" section)
-      courses[i].lastChild.lastChild.insertAdjacentHTML('beforebegin', `
+      courses[i].lastChild.lastChild.insertAdjacentHTML(
+        "beforebegin",
+        `
         <button class="helper-extension helper-remove-button" id="removeCourse${currentCourseID}">
           Remove from this semester
         </button>
-      `)
-      document.getElementById("removeCourse" + currentCourseID).addEventListener("click",function(e){
-        removeCourse(e.target.id.slice(12), courseIDs)
-      })
+      `
+      );
+      document
+        .getElementById("removeCourse" + currentCourseID)
+        .addEventListener("click", function (e) {
+          removeCourse(e.target.id.slice(12), courseIDs);
+        });
     } else {
-
       // Applies to all courses on the page that is not in the list (in "my courses" section)
-      courses[i].lastChild.lastChild.insertAdjacentHTML('beforebegin',`
+      courses[i].lastChild.lastChild.insertAdjacentHTML(
+        "beforebegin",
+        `
         <button class="helper-extension helper-add-button" id="addCourse${currentCourseID}">
           Add to this semester
         </button>
-      `)
-      document.getElementById("addCourse" + currentCourseID).addEventListener("click",function(e){
-        addCourse(e.target.id.slice(9), courseIDs)
-      })
+      `
+      );
+      document
+        .getElementById("addCourse" + currentCourseID)
+        .addEventListener("click", function (e) {
+          addCourse(e.target.id.slice(9), courseIDs);
+        });
     }
   }
 
+  // addcourses buttons for side bars
+  var sidebarlist = document.getElementsByClassName("column c1");
+  for (var i = 0; i < sidebarlist.length; i++) {
+    //  id comes from the ref: https://moodle.hku.hk/course/view.php?id=xxx
+    var included = false;
+    const id = sidebarlist[i].firstChild.href.slice(41);
+    if (courseIDs) {
+      included = courseIDs.includes(id);
+    }
+    const text = sidebarlist[i].removeChild(sidebarlist[i].lastChild);
+    const anchor = document.createElement("span");
+    sidebarlist[i].appendChild(anchor);
+    sidebarlist[i].firstChild.insertAdjacentHTML(
+      "afterEnd",
+      `<div class="helper-extension helper-sidebar-wrapper">
+          <button class="helper-extension helper-sidebar-button" id="sidebarbtn${id}" >${
+        included ? "-" : "+"
+      }</button>
+        </div>`
+    );
+    sidebarlist[i].removeChild(anchor);
+    sidebarlist[i].lastChild.insertAdjacentElement("beforeBegin", text);
 
-  var outerContainer = document.getElementById("frontpage-course-list")
+    if (included) {
+      document
+        .getElementById("sidebarbtn" + id)
+        .addEventListener("click", function (e) {
+          removeCourse(e.target.id.slice(10), courseIDs);
+        });
+    } else {
+      document
+        .getElementById("sidebarbtn" + id)
+        .addEventListener("click", function (e) {
+          addCourse(e.target.id.slice(10), courseIDs);
+        });
+    }
+  }
 
-  if (courseIDs && courseIDs.length){
+  var outerContainer = document.getElementById("frontpage-course-list");
+
+  if (courseIDs && courseIDs.length) {
     //如果有课程
-    outerContainer.insertAdjacentHTML('afterbegin', `
+    outerContainer.insertAdjacentHTML(
+      "afterBegin",
+      `
       <div class="helper-extension course-of-sem-wrapper">
         <h2>
           Course of this semester
@@ -62,44 +112,52 @@ async function addCourseOfSem() {
         </h2>
         <div id="courseOfSem" class="courses frontpage-course-list-enrolled has-pre has-post course-of-sem"></div>
       </div>
-    `)
+    `
+    );
 
-    document.getElementById("removeAll").addEventListener("click", function(){
-      if (confirm("Do you wish to remove all courses from this semester?")){
-        removeAll()
+    document.getElementById("removeAll").addEventListener("click", function () {
+      if (confirm("Do you wish to remove all courses from this semester?")) {
+        removeAll();
       }
-    })
+    });
   } else {
     //没有课程
-    outerContainer.insertAdjacentHTML('afterbegin', `
+    outerContainer.insertAdjacentHTML(
+      "afterbegin",
+      `
       <div class="helper-extension course-of-sem-wrapper">
         <h2>Course of this semester</h2>
         <p><i>Please click 'Add to this semester' on a course to bring it here.</i></p>
       </div>
-    `)
+    `
+    );
   }
-  
 
-  var innerContainer = document.getElementById("courseOfSem")
+  var innerContainer = document.getElementById("courseOfSem");
   for (var i = 0; i < courseHTML.length; i++) {
-    if (i%2) {
+    if (i % 2) {
       //注意这里是偶数 => 这里是不能整除2（i是奇数），但是在显示顺序上是“偶数”
-      courseHTML[i].className = "coursebox clearfix even"
-    }else{
-      courseHTML[i].className = "coursebox clearfix odd"
+      courseHTML[i].className = "coursebox clearfix even";
+    } else {
+      courseHTML[i].className = "coursebox clearfix odd";
     }
 
     // applies to all courses in this semester (in "course of this semester" section)
-    currentCourseID = courseHTML[i].dataset.courseid
-    courseHTML[i].insertAdjacentHTML('afterbegin', `
+    currentCourseID = courseHTML[i].dataset.courseid;
+    courseHTML[i].insertAdjacentHTML(
+      "afterbegin",
+      `
       <a id="removeCourseA${currentCourseID}" style="position: absolute; top: 5px; right: 5px; font-size: 25px; color: darkgrey; cursor: pointer">
         ×
       </a>
-    `)
-    innerContainer.appendChild(courseHTML[i])
-    document.getElementById("removeCourseA" + currentCourseID).addEventListener("click",function(e){
-      removeCourse(e.target.id.slice(13), courseIDs)
-    })
+    `
+    );
+    innerContainer.appendChild(courseHTML[i]);
+    document
+      .getElementById("removeCourseA" + currentCourseID)
+      .addEventListener("click", function (e) {
+        removeCourse(e.target.id.slice(13), courseIDs);
+      });
   }
 }
 
@@ -107,35 +165,35 @@ async function addCourseOfSem() {
 // Helper functions
 
 function clearAll() {
-  var clearElements = document.getElementsByClassName("helper-extension")
+  var clearElements = document.getElementsByClassName("helper-extension");
   //必须倒序删除，因为HTMLCollection会因为remove方法动态变化
   for (var i = clearElements.length - 1; i >= 0; --i) {
-    clearElements[i].remove()
+    clearElements[i].remove();
   }
 }
 
 async function addCourse(courseCode, courseIDs) {
-  if (courseIDs && courseIDs.length){
-    courseIDs.push(courseCode)
-  }else{
-    courseIDs=[courseCode]
+  if (courseIDs && courseIDs.length) {
+    courseIDs.push(courseCode);
+  } else {
+    courseIDs = [courseCode];
   }
-  await browser.storage.sync.set({courseIDs: courseIDs})
-  
-  mainFunction()
+  await browser.storage.sync.set({ courseIDs: courseIDs });
+
+  mainFunction();
 }
 
 async function removeCourse(courseCode, courseIDs) {
-  courseIDs = courseIDs.filter(function(value, index, arr){
+  courseIDs = courseIDs.filter(function (value, index, arr) {
     return value !== courseCode;
   });
-  await browser.storage.sync.set({courseIDs: courseIDs})
-  
-  mainFunction()
+  await browser.storage.sync.set({ courseIDs: courseIDs });
+
+  mainFunction();
 }
 
 async function removeAll() {
-  browser.storage.sync.set({courseIDs: null})
+  browser.storage.sync.set({ courseIDs: null });
 
-  mainFunction()
+  mainFunction();
 }
